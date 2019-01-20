d671fcac481298ae69492c62f7050170b8650fea
Avikal Chhetri <avikalchhetri@yahoo.co.in>
2019-01-20 18:56:02 +0530

    modelling on key_stats dataset

--- code/model_building_with_svm.py
+++ code/model_building_with_svm.py
@@ -1,51 +1,88 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[34]:
 
 
 import numpy as np
 import pandas as pd
-from sklearn import svm
+from sklearn import svm, preprocessing
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
 
-# In[22]:
+# In[52]:
 
 
-def Build_Data_Set(features = ['DE Ratio',
-                               'Trailing P/E']):
-    data_df = pd.DataFrame.from_csv('/Users/avikalchhetri/Desktop/DAT210x/stocks_analyser/key_stats.csv')
+FEATURES =  ['DE Ratio',
+             'Trailing P/E',
+             'Price/Sales',
+             'Price/Book',
+             'Profit Margin',
+             'Operating Margin',
+             'Return on Assets',
+             'Return on Equity',
+             'Revenue Per Share',
+             'Market Cap',
+             'Enterprise Value',
+             'Forward P/E',
+             'PEG Ratio',
+             'Enterprise Value/Revenue',
+             'Enterprise Value/EBITDA',
+             'Revenue',
+             'Gross Profit',
+             'EBITDA',
+             'Net Income Avl to Common ',
+             'Diluted EPS',
+             'Earnings Growth',
+             'Revenue Growth',
+             'Total Cash',
+             'Total Cash Per Share',
+             'Total Debt',
+             'Current Ratio',
+             'Book Value Per Share',
+             'Cash Flow',
+             'Beta',
+             'Held by Insiders',
+             'Held by Institutions',
+             'Shares Short (as of',
+             'Short Ratio',
+             'Short % of Float',
+             'Shares Short (prior ']
+
+def Build_Data_Set():
+    data_df = pd.DataFrame.from_csv('key_stats.csv')
+    data_df = data_df.reindex(np.random.permutation(data_df.index))  #To shuffle the data for training
     #data_df = data_df[:100] #for testing a fewer rows
-    X = np.array(data_df[features].values)
+    X = np.array(data_df[FEATURES ].values)
     y = (data_df['Status']
         .replace('underperform',0)
         .replace('outperform',1)
         .values.tolist()) #Status has two values i.e underperform and outperform. This needs to be converted to
                             # numbers
+        
+    #Preprocessing the data to scale it down
+    X = preprocessing.scale(X)
+    
     return X,y
 
 def Analysis():
+    test_size = 1000
     X, y = Build_Data_Set()
+    print(len(X))
     clf = svm.SVC(kernel='linear', C=1.0)
-    clf.fit(X,y)
+    clf.fit(X[:-test_size],y[:-test_size])
     
-    #Following is for only graphing purposes
-    w = clf.coef_[0]
-    a = -w[0]/w[1]
-    xx = np.linspace(min(X[:,0]),max(X[:,0]))
-    yy = a * xx - clf.intercept_[0] / w[1]
-    h0 = plt.plot(xx, yy, 'k-', label = 'non-weighted')
-    plt.scatter(X[:,0], X[:,1], c = y)
-    plt.ylabel('Trailing PE Ratio')
-    plt.xlabel('BE Ratio')
-    plt.legend()
-    plt.show()
+    correct_count = 0
+    
+    for x in range(1 , test_size+1):
+        if clf.predict(X[-x].reshape(1,-1))[0] == y[-x]:
+            correct_count += 1
+    print("Accuracy:", (correct_count/test_size)*100.00)
 
 
-# In[23]:
+# In[53]:
 
 
 Analysis()
